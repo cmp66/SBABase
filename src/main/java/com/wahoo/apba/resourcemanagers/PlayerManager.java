6d5ea794c40fd02c2f38a25c272e809b034a5186
Carl Phillips <cmp1166@gmail.com>
2025-04-07 21:06:59 -0400

    put latest stuff up

--- src/main/java/com/wahoo/apba/resourcemanagers/PlayerManager.java
+++ src/main/java/com/wahoo/apba/resourcemanagers/PlayerManager.java
@@ -381,7 +381,7 @@ public class PlayerManager
             wkConn = DBUtil.getReadOnlyDBConnection();
             wkStatement = wkConn.prepareStatement(CARDED_LIST_SQL);
             
-            wkStatement.setInt(1, 2019);
+            wkStatement.setInt(1, 2023);
             
             wkResultSet = wkStatement.executeQuery();
             
